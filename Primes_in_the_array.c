5c81675d12012b54e057f44571616853117c91d1
21A91A1262 <97424601+21A91A1262@users.noreply.github.com>
2022-06-05 15:04:39 +0530

    #include<stdio.h> int f(int n) {     int i,fc=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             fc++;         }     }     if(fc==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,x,c=0,i,a[100];     float r=0.0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&x);     for(i=0;i<n;i++)     {         if(f(a[i]))         {
                    c++;
    
            }
        }
        
        printf("%d",c);
    }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int f(int n)
+{
+    int i,fc=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            fc++;
+        }
+    }
+    if(fc==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,x,c=0,i,a[100];
+    float r=0.0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(f(a[i]))
+        {
+        
+                c++;
+
+        }
+    }
+    
+    printf("%d",c);
+}
